d7bfc8c80c2d73e11d2a88e3908cbdc707f3c6f9
TolgaKmbl <tolgakumbul@gmail.com>
2023-04-23 12:45:29 +0300

    Logging Service init

---
+++ LoggingService/src/main/java/com/tolgakumbul/LoggingService/LoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tolgakumbul.LoggingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingServiceApplication.class, args);
+	}
+
+}
---
+++ LoggingService/src/test/java/com/tolgakumbul/LoggingService/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgakumbul.LoggingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
