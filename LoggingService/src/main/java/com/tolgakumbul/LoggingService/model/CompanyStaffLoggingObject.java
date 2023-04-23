460a9aeea69fe9f3feab481a15e0b87bd54cd2fc
TolgaKmbl <tolgakumbul@gmail.com>
2023-04-23 16:00:51 +0300

    added mongodb

---
+++ LoggingService/src/main/java/com/tolgakumbul/LoggingService/model/CompanyStaffLoggingObject.java
@@ -0,0 +1,21 @@
+package com.tolgakumbul.LoggingService.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "CompanyStaff")
+@Data
+public class CompanyStaffLoggingObject {
+
+    @Id
+    private String id;
+
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String isApproved;
+    private LocalDateTime approvalDate;
+}
