702a847fd5c5063343664b71c217bb3e04a3f57e
TolgaKmbl <tolgakumbul@gmail.com>
2023-11-12 14:21:43 +0300

    aggregator service init

---
+++ aggregatorservice/src/main/java/com/tolgakumbul/aggregatorservice/AggregatorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tolgakumbul.aggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorserviceApplication.class, args);
+	}
+
+}
---
+++ aggregatorservice/src/test/java/com/tolgakumbul/aggregatorservice/AggregatorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgakumbul.aggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
